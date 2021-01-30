9d852f491fcd9f2fc40c4405e633da0c5983bff0
Fredrik Domeij <fredrik.domeij@umu.se>
2021-01-30 07:26:06 +0100

    example_errorurl added to IdP list

--- php/test/idp-list.php
+++ php/test/idp-list.php
@@ -30,7 +30,19 @@ $title = "IdP errorURL list";
 <h1><?= $title ?></h1>
 <?php
 
-foreach ($helpdesks as $entityid => $data) {
+$example_errorurl = array(
+	'' => array(
+		'displayname' => array(
+			'sv' => 'GitHub swamid-errorurl example',
+			'en' => 'GitHub swamid-errorurl example',
+		),
+		'errorurl' => 'https://error.swamid.se/default/?errorurl_code=ERRORURL_CODE&errorurl_ts=ERRORURL_TS&errorurl_rp=ERRORURL_RP&errorurl_tid=ERRORURL_TID&errorurl_ctx=ERRORURL_CTX'
+	)
+);
+
+
+
+foreach (array_merge($example_errorurl, $helpdesks) as $entityid => $data) {
 	$descr = $entityid;
 	if (isset($data['displayname']['sv'])) {
 		$desc = $data['displayname']['sv'];
@@ -47,7 +59,7 @@ foreach ($helpdesks as $entityid => $data) {
 	$testurl = "https://error.swamid.se/test/?errorurl=" . urlencode($errorurl);
 
 ?>
-<p><b><?= $desc ?></b> (<?= $entityid ?>)<br>
+<p><b><?= $desc ?></b><?= ($entityid) ? " (" . $entityid . ")" : "" ?><br>
 errorURL: <code><?= $errorurl ?></code><br>
 <a href="<?= $testurl ?>">Test errorURL</a>
 
